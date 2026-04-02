25b83aa90190230f91c7f6a611d81d0aa5ac3c9d
SUBHAM <subhamgiri470@gmail.com>
2026-04-03 01:09:55 +0530

    Import Link component from react-router-dom

--- src/components/ContentOverlay.tsx
+++ src/components/ContentOverlay.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 // Strict TypeScript Interfaces to prevent build errors
 interface Topic { id: string; icon: string; name: string; desc: string; count: string; status: string; }
